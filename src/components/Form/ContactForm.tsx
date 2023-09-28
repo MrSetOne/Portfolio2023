d8eead1035d6a088029da3c3f3f66164bf17d499
Michael Lara Sánchez <lara.sanchez.michael@gmail.com>
2023-09-28 14:46:10 +0200

    emailjs

--- src/components/Form/ContactForm.tsx
+++ src/components/Form/ContactForm.tsx
@@ -1,26 +1,10 @@
-import React, { useState, useRef, useEffect, useCallback } from 'react'
+import React, { useState, useRef, useEffect, useCallback, useMemo } from 'react'
 import './ContactForm.scss'
 import FormInput from './FormInput'
+import FormTextArea from './FormTextArea'
+import { sendForm, send } from '@emailjs/browser'
 
-const EmailInput = () => {
-  const [value, setValue] = useState('')
-
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    const isValid = emailRegex.test(e.target.value)
-  }
-
-
-
-
-  return (
-    <div>
-      <input onChange={handleChange} type="text" placeholder='Email' />
-    </div>
-  )
-}
-
-export type inputState = {
+interface inputState {
   isValid: boolean
   value: string
 }
@@ -30,22 +14,47 @@ const ContactForm = () => {
     isValid: false,
     value: '',
   })
+
+  const serviceID = import.meta.env.PUBLIC_REACT_APP_SERVICE_ID || "";
+  const templateID = import.meta.env.PUBLIC_REACT_APP_YOUR_TEMPLATE_ID || "";
+  const publicKey = import.meta.env.PUBLIC_REACT_APP_PUBLIC_KEY || "";
   
   const [name, setName] = useState<inputState>(inputInitialState)
   const [email, setEmail] = useState<inputState>(inputInitialState)
   const [message, setMessage] = useState<inputState>(inputInitialState)
   const [alertVisible, setAlertVisible] = useState(false)
 
-  const handleSubmit = useCallback((e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
-    setAlertVisible(true)
+  const canSubmit = useMemo(()=>{
+    return name.isValid && email.isValid && message.isValid
   },[name, email, message])
+
+
+  const handleSubmit = useCallback(async(e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    if(!canSubmit) {
+      setAlertVisible(true)
+      return
+    }
+
+    console.log({serviceID, templateID, target:e.target, publicKey});
+
+    const data = {
+      name: name.value,
+      email: email.value,
+      message: message.value
+    }
+
+    await send( serviceID, templateID, data, publicKey);
+    
+
+
+  },[name, email, message, canSubmit])
   
   return (
     <form onSubmit={handleSubmit} className='form__container'>
       <FormInput setState={setName}  placeholder='Nombre' type='text' state={name} alertVisible={alertVisible} />
       <FormInput setState={setEmail}  placeholder='Email' type='email' state={email} alertVisible={alertVisible} />
-      <textarea placeholder='Mensaje' cols={30} rows={10}/>
+      <FormTextArea alertVisible={alertVisible} setState={setMessage} state={message} />
       <div>
         <button>
           Descargar CV
--- src/components/Form/FormTextArea.tsx
+++ src/components/Form/FormTextArea.tsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './FormTextArea.scss'
+import type { inputState } from './ContactForm'
 
-const FormTextArea = () => {
+interface Props {
+  setState: React.Dispatch<React.SetStateAction<inputState>>
+  state: inputState
+  alertVisible: boolean
+}
+
+const FormTextArea = ({ alertVisible, setState, state }:Props) => {
+  
+  const [length, setLength] = useState<number>(0)
+
+  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    console.log('holi');
+    const value = e.target.value
+    const isValid = value !== ''
+    setState((prev) => ({...prev, isValid, value }))
+    setLength(value.length)
+  }
+
   return (
-    <div>
-      <textarea placeholder='Mensaje' cols={30} rows={10}/>
+    <div className='FormTextArea'>
+      <textarea onChange={handleChange} placeholder='Mensaje' rows={10}/>
+      <span>{length}</span>
     </div>
   )
 }
