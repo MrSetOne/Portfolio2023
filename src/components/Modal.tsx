7f47f94cc00d3d4f639a16b081802e2106fae70e
Michael Lara Sánchez <lara.sanchez.michael@gmail.com>
2023-10-18 14:34:42 +0200

    modal desktop

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,6 +1,8 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useMemo } from 'react'
 import "./Modal.scss"
 import { motion, motionValue, useTransform } from 'framer-motion'
+import { useWindowSize } from 'usehooks-ts'
+import ShortArrow from './icons/ShortArrow'
 
 interface Props {
   children?: React.ReactNode
@@ -11,10 +13,13 @@ interface Props {
 const Modal = ({children, imperativeClose}:Props) => {
 
   const y = motionValue(0)
+  const { width, height } = useWindowSize()
 
   const [closing, setClosing] = useState(false)
+  const isDesktop = useMemo(() => width > 600 , [width])
 
-  const variants = {
+
+  const variants = useMemo(()=>({
     open:{
       bottom:0,
       opacity:1
@@ -23,7 +28,7 @@ const Modal = ({children, imperativeClose}:Props) => {
       bottom:'-80vh',
       opacity:0
     }
-  }
+  }),[])
 
   const closeCard = () => {
     setClosing(true)
@@ -33,19 +38,29 @@ const Modal = ({children, imperativeClose}:Props) => {
   }
 
   useEffect(() => y.onChange(latest => {
-    console.log(latest)
     if(latest >= 100) return closeCard()
   }), [])
 
   return (
     <motion.div 
-      className='Modal'
-      initial={{bottom:'-80vh', opacity:0}}
+      className={`Modal ${isDesktop ? 'Modal--desktop' : ''}`}
+      initial={'close'}
       variants={variants}
       animate={closing ? 'close' : 'open'}
+      style={{height: isDesktop ? height : '80vh'}}
     >
-      <motion.div drag='y' dragConstraints={{top:0, bottom:0}} onClick={closeCard} style={{y}} className='Modal__head'>
-        <motion.div />
+      <motion.div 
+        drag='y' 
+        dragConstraints={{top:0, bottom:0}} 
+        onClick={closeCard} 
+        style={{y}} 
+        className='Modal__head'
+      >
+        <motion.div className='Modal__closeLine' />
+        <div className='Modal__closeLine--desktop'>
+          <ShortArrow direction='left' size='.7rem' color='black'/>
+          <span>Cerrar</span>
+        </div>
       </motion.div>
       <motion.div className='Modal__bg' style={{y}}/>
       <motion.div className='Modal__content' style={{y}}>
@@ -86,6 +101,57 @@ const Modal = ({children, imperativeClose}:Props) => {
         <h2>Pruebita Sexy</h2>
         <h2>Pruebita Sexy</h2>
         <h2>Pruebita Sexy</h2>
+        <h2>Pruebita Sexy</h2>
+        <h2>Pruebita Sexy</h2>
+        <h2>Pruebita Sexy</h2>
+        <h2>Pruebita Sexy</h2>
+        <h2>Pruebita Sexy</h2>
+        <h2>Pruebita Sexy</h2>
+        <h2>Pruebita Sexy</h2>
+        <h2>Pruebita Sexy</h2>
+        <h2>Pruebita Sexy</h2>
+        <h2>Pruebita Sexy</h2>
+        <h2>Pruebita Sexy</h2>
+        <h2>Pruebita Sexy</h2>
+        <h2>Pruebita Sexy</h2>
+        <h2>Pruebita Sexy</h2>
+        <h2>Pruebita Sexy</h2>
+        <h2>Pruebita Sexy</h2>
+        <h2>Pruebita Sexy</h2>
+        <h2>Pruebita Sexy</h2>
+        <h2>Pruebita Sexy</h2>
+        <h2>Pruebita Sexy</h2>
+        <h2>Pruebita Sexy</h2>
+        <h2>Pruebita Sexy</h2>
+        <h2>Pruebita Sexy</h2>
+        <h2>Pruebita Sexy</h2>
+        <h2>Pruebita Sexy</h2>
+        <h2>Pruebita Sexy</h2>
+        <h2>Pruebita Sexy</h2>
+        <h2>Pruebita Sexy</h2>
+        <h2>Pruebita Sexy</h2>
+        <h2>Pruebita Sexy</h2>
+        <h2>Pruebita Sexy</h2>
+        <h2>Pruebita Sexy</h2>
+        <h2>Pruebita Sexy</h2>
+        <h2>Pruebita Sexy</h2>
+        <h2>Pruebita Sexy</h2>
+        <h2>Pruebita Sexy</h2>
+        <h2>Pruebita Sexy</h2>
+        <h2>Pruebita Sexy</h2>
+        <h2>Pruebita Sexy</h2>
+        <h2>Pruebita Sexy</h2>
+        <h2>Pruebita Sexy</h2>
+        <h2>Pruebita Sexy</h2>
+        <h2>Pruebita Sexy</h2>
+        <h2>Pruebita Sexy</h2>
+        <h2>Pruebita Sexy</h2>
+        <h2>Pruebita Sexy</h2>
+        <h2>Pruebita Sexy</h2>
+        <h2>Pruebita Sexy</h2>
+        <h2>3</h2>
+        <h2>2</h2>
+        <h2>1</h2>
       </motion.div>
     </motion.div>
   )
