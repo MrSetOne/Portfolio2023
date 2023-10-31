c8b04bd39914b8f798107ee9ecfee00e00f3f298
Michael Lara Sánchez <lara.sanchez.michael@gmail.com>
2023-10-31 09:31:08 +0100

    save

--- src/components/Cards/CCrowmie.tsx
+++ src/components/Cards/CCrowmie.tsx
@@ -9,3 +9,80 @@ const CCrowmie = () => {
 }
 
 export default CCrowmie
+
+
+
+
+
+
+/*
+
+
+?OCUPACIÓN DEL PUESTO
+  * FRONTEND DEVELOPER
+    - MEJORA DE PERFORMANCE EN WEB(ASTROSSR + CACHE) Y APP (HOOKS useMemo useCallback useRef, etc.)
+    - REFACTORIZACIÓN DE CÓDIGO
+    - SEO
+  * UX/UI DESIGNER
+    - ESTUDIO DE FEATURES ANTES DE SU DESARROLLO
+    - DISEÑO DE NUEVAS FEATURES
+    - REDISEÑO DE FEATURES EXISTENTES
+    - ESTANDARIZACIÓN DE ESTILOS
+  
+==================================================
+
+?TECNOLOGIA EMPLEADA
+  * FRONTEND
+    - NODEJS
+    - TYPESCRIPT
+    - REACT
+    - ASTRO
+    - REDUX
+    - I18N
+    - MATERIAL-UI
+    - FRAMER-MOTION
+    - TAILWIND
+    - SASS
+    - chart.js
+    - LEAFLET
+    - GIT + GITHUB
+
+  * UX/UI
+    - FIGMA
+    - MIRO
+
+==================================================
+
+* LOGROS
+
+  - MIGRACIÓN A ASTRO CON SSR
+    - MEJORA DE PERFORMANCE
+      - Comparación de 7s a .3s
+      - Se obtionen los posts desde Wordpress y se renderizan en el servidor
+  - REDISEÑO Y ESTANDARICACIÓN DE APP
+  - KYC
+  - MERCADO P2P
+  - SISTEMA DE REFERIDOS 
+
+==================================================
+
+ENLACES
+  - Web: https://crowmie.com/
+  - App: https://app.crowmie.com/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
