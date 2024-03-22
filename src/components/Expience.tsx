e0f20d63b06ce69b5d53244b4f0b011048cb9fd1
Michael Lara Sánchez <lara.sanchez.michael@gmail.com>
2024-03-22 17:58:33 +0100

    wip: save before change layout

---
+++ src/components/Expience.tsx
@@ -0,0 +1,54 @@
+import React, { useMemo, useRef, useState } from "react";
+import { useWindowSize } from "../hooks/useWindowsSize";
+import "./Expience.scss";
+
+
+const Experience = () => {
+  const firstDot = useRef<HTMLInputElement>(null);
+  const lastDot = useRef<HTMLInputElement>(null);
+
+  const { width } = useWindowSize();
+
+  const lineHeight = useMemo(() => {
+    if (!lastDot.current || !firstDot.current) return 0;
+    console.log('ejecutando');
+    return (
+      lastDot.current.getBoundingClientRect().top -
+      firstDot.current.getBoundingClientRect().top
+    );
+  }, [width]);
+
+  return (
+    <div style={{ overflow: "hidden", position: "relative" }} >
+      <div className="experience__line" style={{ height: lineHeight }} />
+      <ul className="experience__list">
+        <li className="experience__item">
+          <div className="experienceItem__header">
+            <div ref={firstDot} className="dotItem" />
+            <div>
+              <h3>Quasar Dynamics - Frontend developer</h3>
+              <h4>Enero 2024 - Actualidad</h4>
+            </div>
+          </div>
+          <p className="experienceItem__description">
+            Responsable de desarrollar nuevas funcionalidades, mantener el código existente, corregir bugs. También colaboraré con el equipo de backend y diseño para asegurar la cohesión del proyecto. En este puesto pude probar a trabajar en varios proyectos de forma simultanea, cumpliendo con los plazos y objetivos establecidos.
+          </p>
+        </li>
+        <li className="experience__item">
+          <div className="experienceItem__header">
+            <div ref={lastDot} className="dotItem" />
+            <div>
+              <h3>Crowmie - Frontend developer & UX/UI</h3>
+              <h4>Diciembre 2022 - Septiembre 2023</h4>
+            </div>
+          </div>
+          <p className="experienceItem__description">
+            Responsable del completo ciclo de vida de las nuevas funcionalidades. Estudiaba su viabilidad, realizaba el diseño, desarrollaba el código, y desplegaba las nuevas funcionalidades, siguiendo los principios SOLID para asegurar un código limpio, mantenible y escalable.
+          </p>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Experience;
